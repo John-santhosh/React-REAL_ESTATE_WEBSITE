59de980a18874fd05f51b0961456525f35a6bf5a
john <johnsantosh2@gmail.com>
2023-06-03 14:03:14 +0530

    home page layout

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,9 +1,112 @@
 import styled from "styled-components";
+import logo from "../assets/skyreal.svg";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import { navLinks } from "../data";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <Wrapper>Footer</Wrapper>;
+  return (
+    <Wrapper>
+      <div className=" contacts">
+        <div className=" section-center gap-4 flex-wrap d-flex justify-content-between py-4 py-md-5">
+          <div className="">
+            <h2>Do you have any questions ?</h2>
+            <p>We&#39;ll help you to grow your career and growth.</p>
+          </div>
+          <button className="btn rounded-5 px-5">
+            <Link to="/contact">Contact Us today</Link>
+          </button>
+        </div>
+      </div>
+      <div className="footer page-footer font-small blue pt-4 ">
+        <div className="container-fluid  text-md-left section-center">
+          <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3 text-start">
+              <img src={logo} alt="logo" className="logo" />
+              <h3 className="my-3">Do You Need Help With Anything?</h3>
+              <p className="mb-3">
+                Receive updates, hot deals, tutorials, discounts sent straignt
+                in your inbox every month
+              </p>
+              <InputGroup className="mb-3">
+                <Form.Control
+                  placeholder="Recipient's username"
+                  aria-label="Recipient's username"
+                  aria-describedby="basic-addon2"
+                />
+                <Button variant="outline-secondary" id="button-addon2">
+                  Button
+                </Button>
+              </InputGroup>
+            </div>
+
+            <hr className="clearfix w-100 d-md-none pb-0" />
+
+            <div className="col-md-3 mb-md-0 mb-3">
+              <h5 className="text-uppercase">Layouts</h5>
+              <ul className="list-unstyled ">
+                {navLinks.map(({ id, url, text }) => {
+                  return (
+                    <p key={id} className="my-3">
+                      <Link to={url}>{text} Page</Link>
+                    </p>
+                  );
+                })}
+              </ul>
+            </div>
+
+            <div className="col-md-3 mb-md-0 mb-3">
+              <h5 className="text-uppercase">All Sections</h5>
+              <ul className="list-unstyled">
+                <li className="my-3">
+                  <a href="#!">Headers</a>
+                </li>
+                <li className="my-3">
+                  <a href="#!">Features</a>
+                </li>
+                <li className="my-3">
+                  <a href="#!">Attractive</a>
+                </li>
+                <li className="my-3">
+                  <a href="#!">Testimonials</a>
+                </li>
+                <li className="my-3">
+                  <a href="#!">Videos</a>
+                </li>
+                <li className="my-3">
+                  <a href="#!">Footers</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="footer-copyright text-center py-3">
+          © {new Date().getFullYear()} Copyright:
+          <a href="#"> skyReal</a>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
-const Wrapper = styled.footer``;
+const Wrapper = styled.footer`
+  background-color: var(--clr-p-9);
+  .logo {
+    width: 150px;
+  }
+  .contacts {
+    background-color: var(--clr-p-10);
+  }
+  .footer {
+    background-color: var(--clr-p-8);
+    color: #fff;
+  }
+  h2 {
+    /* color: #fff; */
+  }
+`;
 
 export default Footer;
--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,8 +1,101 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { navLinks } from "../data";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/skyreal.svg";
+import { FaRegUserCircle } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 
+import Offcanvas from "react-bootstrap/Offcanvas";
 const Header = () => {
-  return <Wrapper>Header</Wrapper>;
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <Wrapper className="py-3">
+      <div className="section-center ">
+        <div className="logo  ">
+          <Link to="/" className="logo-img">
+            <img src={logo} alt="logo" />
+          </Link>
+
+          <button onClick={handleShow} className=" d-md-none">
+            <GiHamburgerMenu />
+          </button>
+
+          <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <div>
+                {navLinks.map(({ id, url, text }) => {
+                  return (
+                    <p key={id} className="my-3" onClick={handleClose}>
+                      <NavLink to={url}>{text}</NavLink>
+                    </p>
+                  );
+                })}
+                <div className="login btn btn-100 mt-3" onClick={handleClose}>
+                  <FaRegUserCircle className="me-3" />
+                  Login
+                </div>
+              </div>
+            </Offcanvas.Body>
+          </Offcanvas>
+        </div>
+        <ol className="p-0 m-0 gap-4 d-md-flex d-none">
+          {navLinks.map(({ id, url, text }) => {
+            return (
+              <li key={id}>
+                <NavLink to={url}>{text}</NavLink>
+              </li>
+            );
+          })}
+        </ol>
+        <div className="login btn btn-solid  d-md-block d-none">
+          <FaRegUserCircle className="me-3" />
+          Login
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
-const Wrapper = styled.nav``;
+const Wrapper = styled.nav`
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  .logo {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-items: end;
+    align-items: center;
+    svg {
+      font-size: 1.5rem;
+      color: var(--clr-p-6);
+    }
+  }
+  .logo-img {
+    width: 150px;
+    justify-self: start;
+  }
+  @media screen and (min-width: 768px) {
+    > div {
+      display: grid;
+      grid-template-columns: 1fr 4fr 1fr;
+      gap: 1rem;
+      height: 3.5rem;
+      align-items: center;
+      justify-items: center;
+      > * {
+        /* background-color: olive; */
+      }
+    }
+  }
+  ol {
+    list-style: none;
+  }
+`;
 
 export default Header;
